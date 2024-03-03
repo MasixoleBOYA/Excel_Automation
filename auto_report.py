347e50d190b858182c69d9b3690c3aaea920f957
MasixoleBoya <tshollyboya00@gmail.com>
2024-03-03 20:28:13 +0000

    first two functions done

--- auto_report.py
+++ auto_report.py
@@ -1,7 +1,7 @@
 # from openpyxl import Workbook, load_workbook
 
 
-# wb = load_workbook('') # Give path, unless in same dir 
+# work_book = load_workbook('') # Give path, unless in same dir 
 # ws = wb.active
 
 # print(wb.sheetnames)
@@ -12,7 +12,24 @@ depots = ["Alrode",
           "Waltloo",
           "Port Elizabeth"]
 
+
 for i in depots:
-    print(i)
+    work_sheet = work_book[i]
+    column = work_sheet['Gantry AP']
+
+    for j in column:
+        j.value = i
+
+
+# SECOND PART
+appending_sheet = work_book["Alrode"]
+
+for depot_name in depots[1:]:
+    current_worksheet = work_book[depot_name]
+    for row in current_worksheet.iter_rows(min_row =2,
+                                           max_row = current_worksheet.max_rows,
+                                           values_only = True):
+        appending_sheet.append(row)
+
 
 # wb.save('xxx.xlsx')
