9ab308b4defc5b40a465624ba12989f885bd7a4d
Masixole Boya <tshollyboya00@gmail.com>
2024-03-04 12:12:02 +0200

    Update auto_report.py

--- auto_report.py
+++ auto_report.py
@@ -7,29 +7,44 @@
 # print(wb.sheetnames)
 
 depots = ["Alrode",
-          "Island View Terminal", 
+          "Bethlehem",
+          "Cape Town",
+          "East London",
+          "Island View",
+          "Klerksdorp",
+          "Ladysmith",
+          "Mossel Bay",
+          "Nelspruit",
+          "Port Elizabeth",
           "Sasolburg",
+          "Tarlton",
           "Waltloo",
-          "Port Elizabeth"]
+          "Witbank"]
 
 
-for i in depots:
-    work_sheet = work_book[i]
-    column = work_sheet['Gantry AP']
-
-    for j in column:
-        j.value = i
+# PART 1: renames the "Gantry AP" column for all sheets
+def sheet_rename()->None:
+          for i in depots:
+              work_sheet = work_book[i]
+              column = work_sheet['Gantry AP']
+          
+              for j in column:
+                  j.value = i
 
 
 # SECOND PART
-appending_sheet = work_book["Alrode"]
+def appending_to_onesheet(sheetName: str) -> None:
+          appending_sheet = work_book[sheetName]
+          
+          for depot_name in depots[1:]:
+              current_worksheet = work_book[depot_name]
+              for row in current_worksheet.iter_rows(min_row =2,
+                                                     max_row = current_worksheet.max_rows,
+                                                     values_only = True):
+                  appending_sheet.append(row)
 
-for depot_name in depots[1:]:
-    current_worksheet = work_book[depot_name]
-    for row in current_worksheet.iter_rows(min_row =2,
-                                           max_row = current_worksheet.max_rows,
-                                           values_only = True):
-        appending_sheet.append(row)
-
-
-# wb.save('xxx.xlsx')
+if __name__ == '__main__':
+          sheet_rename()
+          appending_to_onesheet("Alrode")
+          
+          # wb.save('xxx.xlsx')
