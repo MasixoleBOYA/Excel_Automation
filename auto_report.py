87b6f930c44c8b7331fef4d97e0c3644a5297b58
Masixole Boya <tshollyboya00@gmail.com>
2024-03-06 16:54:30 +0200

    Update auto_report.py

--- auto_report.py
+++ auto_report.py
@@ -1,18 +1,7 @@
-'''
-C:\Users\J1121857\OneDrive - TotalEnergies\Desktop>pip install openpyxl
-WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ConnectTimeoutError(<pip._vendor.urllib3.connection.HTTPSConnection object at 0x000001DAD9F5CF20>, 'Connection to pypi.org timed out. (connect timeout=15)')': /simple/openpyxl/
-WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ConnectTimeoutError(<pip._vendor.urllib3.connection.HTTPSConnection object at 0x000001DADD6E2750>, 'Connection to pypi.org timed out. (connect timeout=15)')': /simple/openpyxl/
-WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ConnectTimeoutError(<pip._vendor.urllib3.connection.HTTPSConnection object at 0x000001DADD7AF8F0>, 'Connection to pypi.org timed out. (connect timeout=15)')': /simple/openpyxl/
-WARNING: Retrying (Retry(total=1, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ConnectTimeoutError(<pip._vendor.urllib3.connection.HTTPSConnection object at 0x000001DADD7AFB00>, 'Connection to pypi.org timed out. (connect timeout=15)')': /simple/openpyxl/
-WARNING: Retrying (Retry(total=0, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ConnectTimeoutError(<pip._vendor.urllib3.connection.HTTPSConnection object at 0x000001DADD7AFD10>, 'Connection to pypi.org timed out. (connect timeout=15)')': /simple/openpyxl/
-ERROR: Could not find a version that satisfies the requirement openpyxl (from versions: none)
-ERROR: No matching distribution found for openpyxl
-'''
-
-# from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook, load_workbook
 
 
-# work_book = load_workbook('') # Give path, unless in same dir 
+work_book = load_workbook('C:/Users/J1121857/Downloads/GANTRY_RAW_data.xlsx') # Give path, unless in same dir 
 # ws = wb.active
 
 # print(wb.sheetnames)
@@ -21,41 +10,67 @@ depots = ["Alrode",
           "Bethlehem",
           "Cape Town",
           "East London",
-          "Island View",
+          #"Island View",
           "Klerksdorp",
           "Ladysmith",
           "Mossel Bay",
           "Nelspruit",
           "Port Elizabeth",
           "Sasolburg",
-          "Tarlton",
+          #"Tarlton",
           "Waltloo",
           "Witbank"]
 
 
 # PART 1: renames the "Gantry AP" column for all sheets
 def sheet_rename()->None:
-          for i in depots:
-              work_sheet = work_book[i]
-              column = work_sheet['Gantry AP']
-          
-              for j in column:
-                  j.value = i
+    for i in work_book.sheetnames: 
+        work_sheet = work_book[i] #for each depot, consider the sheet that matches the depot
+        column_index = 1
 
+        for x in work_sheet.iter_rows(min_row=2, max_row=work_sheet.max_row, min_col=column_index, max_col=column_index):
+            for cell in x:
+                cell.value = i
+
+
+
+        # for col in work_sheet.iter_cols(min_row=1,max_row=1,min_col=1):
+            # print(col[0].value)
+            # print(col[0].column)
+            # if col[0].value == "Gantry AP":    
+            #     column_index = col[0].column
+            #     working_column = col[0].value
+            #     # break
+                     
+            # print("x1")
+            # column = work_sheet[column_index]
+            # print("x2")
+
+
+
+            
+
+
+            # for j in column:
+            #     # print(f"VALUES OF COLUMN ARE: \n{j.value}")
+            #     j.value = i
+            # print("x3")
+
+          
 
 # SECOND PART
 def appending_to_onesheet(sheetName: str) -> None:
-          appending_sheet = work_book[sheetName]
+    appending_sheet = work_book[sheetName]
           
-          for depot_name in depots[1:]:
-              current_worksheet = work_book[depot_name]
-              for row in current_worksheet.iter_rows(min_row =2,
-                                                     max_row = current_worksheet.max_rows,
+    for depot_name in depots[1:]:
+        current_worksheet = work_book[depot_name]
+        for row in current_worksheet.iter_rows(min_row =2,
+                                                     max_row = current_worksheet.max_row,
                                                      values_only = True):
-                  appending_sheet.append(row)
+            appending_sheet.append(row)
 
-if __name__ == '__main__':
-          sheet_rename()
-          appending_to_onesheet("Alrode")
+print(work_book.sheetnames)
+sheet_rename()
+appending_to_onesheet("Alrode")
+work_book.save('C:/Users/J1121857/Downloads/TEST_NEW_NEW.xlsx')
           
-          # wb.save('xxx.xlsx')
